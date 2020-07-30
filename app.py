2d7f7738431ba101523d03aa643b1dc6048cd59c
Jacobcoding15 <jacob.nelson@bklynlab.org>
2020-07-30 11:46:49 -0400

    some message

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
-
-
+from flask import render_template
+from flask import request
+from model import get_info
+from datetime import datetime
 # -- Initialization section --
 app = Flask(__name__)
-
-
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template('index.html', time=datetime.now())
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+    print(request.form)
+    user_name=(request.form["character_name"])
+    
+    info = get_info(user_name)
+    return render_template("results.html", user_name=user_name, info=info, time=datetime.now())
---
+++ model.py
@@ -0,0 +1,131 @@
+def get_info(character):
+    if character == "Batman":
+        return "In the name of his murdered parents, Bruce Wayne wages eternal war on the criminals of Gotham City. He is vengeance. He is the night. He is Batman. One of the most iconic fictional characters in the world, Batman has dedicated his life to an endless crusade, a war on all criminals in the name of his murdered parents, who were taken from him when he was just a child. Since that tragic night, he has trained his body and mind to near physical perfection to be a self-made Super Hero. He's developed an arsenal of technology that would put most armies to shame. And he's assembled teams of his fellow DC Super Heroes, like the Justice League, the Outsiders and Batman, Incorporated. A playboy billionaire by day, Bruce Wayne’s double life affords him the comfort of a life without financial worry, a loyal butler-turned-guardian and the perfect base of operations in the ancient network of caves beneath his family’s sprawling estate. By night, however, he sheds all pretense, dons his iconic scalloped cape and pointed cowl and takes to the shadowy streets, skies and rooftops of Gotham City."
+    elif character == "Superman":
+        return "The powers of the DC Comics character Superman have changed a great deal since his introduction in the 1930s. As the character developed, his abilities were enhanced in order to maintain the interest of his audience. The extent of his powers peaked during the 1960s and - albeit to a lesser extent - the 1970s and 1980s, to the point where it became difficult to create suitable challenges for the character. As a result, his powers were significantly reduced when his story was rebooted by writer John Byrne after the Crisis on Infinite Earths series. After Byrne's departure, Superman's powers were gradually increased again, although he still remains weaker than his Pre-Crisis incarnation. "
+    elif character == "Spiderman":
+        return "In Spider-Man's first story, in Marvel Comics' Amazing Fantasy, no. 15 (1962), American teenager Peter Parker, a poor sickly orphan, is bitten by a radioactive spider. As a result of the bite, he gains superhuman strength, speed, and agility along with the ability to cling to walls.Spider-Man has spider-like abilities including superhuman strength and the ability to cling to most surfaces. He is also extremely agile and has amazing reflexes. Spider-Man also has a “spider sense,” that warns him of impending danger. Spider-Man has supplemented his powers with technology."
+    elif character == "Thor":
+        return "He's the Norse God of Thunder. He was a religious figure back in the days when the Roman Empire was a thing. ... Blake flees into a cave, finds a walking stick, strikes it against a rock… and is suddenly transformed into Thor, the God of Thunder."
+    elif character == "Ironman":
+        return "Young Tony Stark was a prodigy of mechanical engineering genius. ... Tony escaped with James Rhodes (now War Machine) and returned to America to become a part of the Avengers, taking his fathers teachings of giving back to the world to heart and using his new armor to aid mankind."
+    elif character == "Captain America":
+        return "Captain America is the alter ego of Steve Rogers, a frail young man enhanced to the peak of human perfection by an experimental serum to aid the United States government's efforts in World War II. Near the end of the war, he was trapped in ice and survived in suspended animation until he was revived in modern times."
+    elif character == "Hulk":
+        return "Dr. Bruce Banner lives a life caught between the soft-spoken scientist he's always been and the uncontrollable green monster powered by his rage. Exposed to heavy doses of gamma radiation, scientist Bruce Banner transforms into the mean, green rage machine called the Hulk."
+    elif character == "Flash":
+        return "Barry Allen's origin is that he was struck by a bolt of lightning that connected him with a mysterious source of power he dubbed the “Speed Force.” Originally created by Harry Lampert and Gardner Fox in Flash Comics #1 in 1940, “The Flash” wasn't Barry Allen when he first appeared."
+    elif character == "Wonder Woman":
+        return "Wonder Woman tells the tale of Princess Diana of Themyscira. Her original origin story says she was formed out of clay by her mother, Hippolyta, and had life bestowed upon her by the Greek gods — making her the only Amazon not conceived by a man."
+    elif character == "Green Lantern":
+        return "Green Lantern was Hal Jordan, a test pilot who was given a power ring by a dying alien, Abin Sur, and who became a member of the Green Lantern Corps, an interstellar organization of police overseen by the Guardians of the Universe."
+    elif character == "Wolverine":
+        return "Wolverine was born as James Howlett in northern Alberta, Canada, (approximately near Cold Lake) during the late 1880s, purportedly to rich farm owners John and Elizabeth Howlett, though he is actually the illegitimate son of the Howletts' groundskeeper, Thomas Logan."
+    elif character == "Silver Surfer":
+        return "The Silver Surfer is a humanoid alien with metallic skin who can travel through space with the aid of his surfboard-like craft. Originally a young astronomer named Norrin Radd on the planet Zenn-La, he saved his homeworld from the planet devourer, Galactus, by serving as his herald."
+    elif character == "Catwoman":
+        return "Catwoman, then called the Cat, first appeared in Batman #1 (Spring 1940) as a mysterious burglar and jewel thief, revealed at the end of the story to be a young, attractive (unnamed) woman, having disguised herself as an old woman during the story and been hired to commit a burglary."
+    elif character == "Doctor Strange":
+        return "Strange's backstory was first told in Strange Tales #115. At first, he was a talented surgeon with an asshole streak, winning fame and fortune through his work. But a car crash crippled his hands, ruining his career. He spent years looking for a miracle treatment that would fix him, and eventually wound up in Tibet."
+    elif character == "Professor X":
+        return "Charles Xavier, better known as Professor X, is one of the most important figures in the Marvel Universe. First appearing in 1963's X-Men #1, Xavier is a mutant and the world's most powerful telepath. He dreams of a world where mutants and humans can live in peace. However, this puts him in the impossible position of combating not only the bigotry of frightened humans but the anger of mutants fighting to dominate the rest of the world. To help achieve his goals, Xavier founds his School for Gifted Youngsters, leading to the birth of the X-Men, the New Mutants, and by extension, a whole host of spinoff teams like X-Force, X-Factor, Generation X, and more. "
+    elif character == "Cyclops":
+        return "In Hesiod the Cyclopes were three sons of Uranus and Gaea—Arges, Brontes, and Steropes (Bright, Thunderer, Lightener)—who forged the thunderbolts of Zeus. Later authors made them the workmen of Hephaestus and said that Apollo killed them for making the thunderbolt that slew his son Asclepius."
+    elif character == "Pheonix":
+        return "The Phoenix is a mythical bird from Ancient Greek legends. The story goes that the bird lives for several hundred years before it dies by setting itself on fire. However, it then starts a new life from the ashes of the fire. ... The Phoenix is often referred to as a fire bird, because it dies and is reborn out of fire."
+    elif character == "Mr.Fantastic":
+        return "He is the inventor of the spacecraft that was bombarded by cosmic radiation on its maiden voyage, granting the Fantastic Four their powers. Richards gained the ability to stretch his body into any shape he desires."
+    elif character == "Kid Flash":
+        return "Wally West is the biological son of Rudy and Marry West. He grew up a normal kid in Central City until his aunt Iris married Barry Allen. Not long afterward Wally discovered that his uncle was non other then the hero of central city, The Flash."
+    elif character == "Spawn":
+        return "In his early battles, Spawn faces street thugs and gangs, becoming a dark and brutal antihero, culminating in the brutal murder of a pedophile and child murderer named Billy Kincaid. ... There he meets the bum Cogliostro, who seems to know much more about Spawn than he first lets on, and becomes his mentor."
+    elif character == "Doctor Doom":
+        return "Victor von Doom was born in a Romani camp outside Haasenstadt, Latveria. His mother, Cynthia, was killed in a bargain with Mephisto that went wrong. Despite his father's attempts to care for her, a noblewoman died and Werner von Doom fled with a young Victor on a cold winter's night."
+    elif character == "Martian Manhunter":
+        return "One of the last survivors of the civil war that ravaged his home planet of Mars, J'onn J'onnz traveled to Earth and now fights to protect his adoptive home. J'onn J'onzz of the planet Mars was accidentally transported to Earth, where he became one of its greatest champions under the moniker Martian Manhunter."
+    elif character == "Galactus":
+        return 
+    elif character == "Magneto":
+        return 
+    elif character == "Nightwing":
+        return 
+    elif character == "Hawkeye":
+        return 
+    elif character == "Lex Luthor":
+        return 
+    elif character == "Scarlet Witch":
+        return 
+    elif character == "Joker":
+        return 
+    elif character == "Shazam":
+        return 
+    elif character == "Deadpool":
+        return 
+    elif character == "Thanos":
+        return 
+    elif character == "Darkseid":
+        return 
+    elif character == "Beast":
+        return 
+    elif character == "Punisher":
+        return 
+    elif character == "Rogue":
+        return 
+    elif character == "John Constantine":
+        return 
+    elif character == "Green Goblin":
+        return 
+    elif character == "Red Hood":
+        return 
+    elif character == "Doctor Octopus":
+        return 
+    elif character == "Lois Lane":
+        return 
+    elif character == "":
+        return 
+    elif character == "Green Lantern":
+        return 
+    elif character == "Green Lantern":
+        return 
+    elif character == "Green Lantern":
+        return 
+    elif character == "Green Lantern":
+        return 
+    elif character == "Green Lantern":
+        return 
+    elif character == "Green Lantern":
+        return 
+    elif character == "Green Lantern":
+        return 
+    elif character == "Green Lantern":
+        return 
+    elif character == "Green Lantern":
+        return 
+    elif character == "Green Lantern":
+        return 
+    elif character == "Green Lantern":
+        return 
+    elif character == "Green Lantern":
+        return 
+    elif character == "Green Lantern":
+        return 
+    elif character == "Green Lantern":
+        return 
+    elif character == "Green Lantern":
+        return 
+    elif character == "Green Lantern":
+        return 
+    elif character == "Green Lantern":
+        return 
+    elif character == "Green Lantern":
+        return 
+    elif character == "Green Lantern":
+        return 
+    elif character == "Green Lantern":
+        return 
+    elif character == "Green Lantern":
+        return 
+    elif character == "Green Lantern":
+        return 
+    elif character == "Green Lantern":
+        return 
